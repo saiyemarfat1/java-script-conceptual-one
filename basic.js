075395ca42e1fb62efd2f398d2a704922af8137d
Saiyem Arfat <saiyemarfat775@gmail.com>
2023-07-31 22:48:57 +0600

    first commit

---
+++ basic.js
@@ -0,0 +1,95 @@
+// console.log("Hello World!");
+// console.log(5);
+
+// variable
+
+// var x = 5; 
+// x = 10;
+
+// console.log(x);
+
+
+//  variable naming
+// valid name
+// var x = 5;
+// var age = 25;
+// var name = "mehedy";
+// var name1 = "dhfao";
+// var $helllo = "dosf a";
+
+// invalid name
+// var 1hello ;
+// var my-age;
+// var if;
+
+
+// data tyeps
+// var age = 25; //number 
+// var name = 'Mehedy' // string
+// var x = true; // boolean
+
+// var x = 25.4565;
+// var y = parseInt(x);
+
+// console.log(y);
+
+// var num = "15.36";
+// var num1 = parseFloat(num);
+// console.log(parseInt(num1))
+
+// var number = "25.43254";
+// console.log(typeof number);
+
+// var floathing = parseFloat(number);
+// var integer = parseInt(floathing);
+// var fixed = floathing.toFixed(2);
+
+// console.log(floathing);
+// console.log(integer);
+// console.log(parseFloat(fixed));
+
+
+
+// var x = 5;
+// var y = 10;
+// var z = x + y;
+
+// var x = x + 5;
+// console.log(x)
+
+// console.log(z);
+
+// var x = 5;
+// x = x +1
+// x +=1
+// x++ // post increment
+// ++x // pre increment
+
+
+// var x = 5;
+// console.log(x++); // print hocce 5 tarpor batece 1 = 6 
+// console.log(x++); // print hocce 5 tarpor batece 1 = 7 
+
+// var y = 10;
+// console.log(++y)
+// console.log(++y)
+
+// array
+// var arr = [10, 15, 20, 30]
+// console.log(arr.length)
+// var num1 = arr[0]
+// console.log(num1)
+
+
+var hasan = ["sadia","nadia"]
+
+hasan.push("jorina")
+hasan.push("sokina")
+hasan.push("sumaiya")
+
+console.log(hasan)
+var ex = hasan.pop()
+hasan.pop()
+hasan.push("daliya")
+console.log(hasan)
+console.log(ex)
