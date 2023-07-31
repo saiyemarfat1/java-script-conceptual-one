32bc934e88e1267e2d371956d000b26aff913121
Saiyem Arfat <saiyemarfat775@gmail.com>
2023-07-31 23:48:21 +0600

    second

--- basic.js
+++ basic.js
@@ -3,7 +3,7 @@
 
 // variable
 
-// var x = 5; 
+// var x = 5;
 // x = 10;
 
 // console.log(x);
@@ -24,7 +24,7 @@
 
 
 // data tyeps
-// var age = 25; //number 
+// var age = 25; //number
 // var name = 'Mehedy' // string
 // var x = true; // boolean
 
@@ -67,8 +67,8 @@
 
 
 // var x = 5;
-// console.log(x++); // print hocce 5 tarpor batece 1 = 6 
-// console.log(x++); // print hocce 5 tarpor batece 1 = 7 
+// console.log(x++); // print hocce 5 tarpor batece 1 = 6
+// console.log(x++); // print hocce 5 tarpor batece 1 = 7
 
 // var y = 10;
 // console.log(++y)
@@ -81,15 +81,121 @@
 // console.log(num1)
 
 
-var hasan = ["sadia","nadia"]
+// var hasan = ["sadia","nadia"]
 
-hasan.push("jorina")
-hasan.push("sokina")
-hasan.push("sumaiya")
+// hasan.push("jorina")
+// hasan.push("sokina")
+// hasan.push("sumaiya")
 
-console.log(hasan)
-var ex = hasan.pop()
-hasan.pop()
-hasan.push("daliya")
-console.log(hasan)
-console.log(ex)
+// console.log(hasan)
+// var ex = hasan.pop()
+// hasan.pop()
+// hasan.push("daliya")
+// console.log(hasan)
+// console.log(ex)
+
+
+// are you adult or not?
+// var age = 25;
+
+// if(age > 18){
+// console.log("you are adult");
+// }
+// else{
+// console.log("tumi bacca aco");
+// }
+
+// var mark = 57;
+
+// if(mark > 80){
+//     console.log("you got A+")
+// }
+// else if(mark > = 70){
+//     console.log("you got A")
+// }
+// else if(mark > 60){
+//     console.log("you got B")
+// }
+// else if(mark > 50){
+//     console.log("you got C")
+// }
+// else if{
+//     console.log("tumi fail.next")
+// }
+
+
+
+
+// var temperature = 25;
+// var isRainning = false;
+
+// if(temperature > 30){
+//     console.log("It is a hot day")
+//     if(isRainning == true){
+//         console.log("but it is rainning")
+//     }else{
+//         console.log("it is not rainning")
+//     }
+// }
+// else{
+//     console.log("It is a cold day")
+//     if(isRainning){
+//         console.log("but it is rainning")
+//     }else{
+//         console.log("and it is not rainning")
+//     }
+// }
+
+
+// console.log("Um sorry...")
+// console.log("Um sorry...")
+// console.log("Um sorry...")
+// console.log("Um sorry...")
+// console.log("Um sorry...")
+// console.log("Um sorry...")
+// console.log("Um sorry...")
+// console.log("Um sorry...")
+// console.log("Um sorry...")
+
+// var i = 1;
+// while(i<=100){
+//     console.log("Um sorry..." ,i)
+//     // i++
+//     i = i + 1;
+// }
+
+// for(var x = 1; x<=10; x++){
+//     console.log("Um sorry..." ,x)
+// }
+
+var arr = [10, 20, 30, 40, 30, 50]
+
+// console.log(arr.indexOf(40))
+
+// console.log(arr.indexOf(100))
+
+// var x = 0;
+// while(x<arr.length){
+//     console.log(arr[x]);
+//     x++
+// }
+
+// for (var i = 0; i < arr.length; i++) {
+//     console.log(arr[i])
+//     if (arr[i] === 30){
+//         break;
+//     }
+   
+    
+// }
+
+
+// for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] === 30){
+//         continue;
+//     }
+//     console.log(arr[i])
+    
+// }
+
+
